f49d1c6bcfd44a03ffc383942397612435320329
Prettiest-star <94059288+Prettiest-star@users.noreply.github.com>
2022-08-24 22:13:38 -0400

    spook

---
+++ app.py
@@ -0,0 +1,13 @@
+#Version 2.5
+#Lucita Sharp
+#December 1st
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
