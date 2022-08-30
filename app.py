5161ac1b4ccbf1574439cc9e81f8f1bea3837d34
Prettiest-star <94059288+Prettiest-star@users.noreply.github.com>
2022-08-29 20:44:14 -0400

    officially done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Version 3
+#Version 2.5
 #Lucita Sharp
 #December 1st
 from flask import Flask, render_template
@@ -10,4 +10,4 @@ def home():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
